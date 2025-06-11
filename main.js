f9b195ad3e6d30f1a45987d864c8299cbcfecde2
Jorge Munera <gerjo9211@hotmail.com>
2025-06-11 12:42:17 -0500

    commit inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+// aqui van las importaciones de las funciones
+
+// --------- funcion sumar
+
+
+// --------- funcion restar
+
+console.log("este es un cambio que quiero")
+
+// --------- funcion multiplicar
