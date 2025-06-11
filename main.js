d5d8c0a9bb023da449a7042ad09e7e9ec7704143
Jorge Munera <gerjo9211@hotmail.com>
2025-06-11 14:13:30 -0500

    consolidacion funciones

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // aqui van las importaciones de las funciones
 
 // --------- funcion sumar
-
+const suma = require("./suma");
 
 // --------- funcion restar
+const resta = require("./resta");
 
 
 // --------- funcion multiplicar
